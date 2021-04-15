4741eb21a1977b6d982d7b1ff013a5d417d4be9d
thomazz-nl <thomazz-nl@users.noreply.github.com>
2021-04-15 15:53:32 +0200

    allow custom tvheadend port, scheme (https) and path

--- horepg/tvheadend.py
+++ horepg/tvheadend.py
@@ -7,13 +7,25 @@ Fetch a list of channels from TVHeadend
 import requests
 import socket
 from requests.auth import HTTPDigestAuth
+from urllib.parse import urlparse
 
 def tvh_get_channels(host, port=9981, username='', password=''):
   channels = []
-  r = requests.get('http://{:s}:{:d}/api/channel/list'.format(host, port), auth=(username, password))
+
+  try:
+    host = host if host.startswith('http://') or host.startswith('https://') else '//' + host
+    url_object = urlparse(host, scheme='http')
+  except ValueError:
+    port = 9981
+
+  if url_object.port:
+      port = url_object.port
+  
+  request_uri = '{:s}://{:s}:{:d}{:s}/api/channel/list'.format(url_object.scheme, url_object.hostname, port, url_object.path)
+  r = requests.get(request_uri, auth=(username, password))
   if r.status_code == 401:
     # Retry with HTTP digest authentication
-    r = requests.get('http://{:s}:{:d}/api/channel/list'.format(host, port), auth=HTTPDigestAuth(username, password))
+    r = requests.get(request_uri, auth=HTTPDigestAuth(username, password))
   if r.status_code != 200:
     raise Exception('connection to tvheadend failed with status {:d}'.format(r.status_code))
   data = r.json()
